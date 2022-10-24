70b75664490725d9a79aa5b9b814d0eeb8c84970
Jack <jack.allen@above-uav.com>
2022-10-24 10:30:40 -0700

    produce material ui import warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 
+import { Table, TableHead, TableBody, TableContainer, TableRow, TableCell } from "@mui/material";
+
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Field Name</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
